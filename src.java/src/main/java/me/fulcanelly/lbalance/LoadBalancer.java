0fed88b2ffa7be2f56d9ab864774416504e303a2
fulcanelly <dallasxfulcanelly@gmail.com>
2022-01-19 02:10:28 +0200

    add blueprint for balancer front end

---
+++ src.java/src/main/java/me/fulcanelly/lbalance/LoadBalancer.java
@@ -0,0 +1,14 @@
+package me.fulcanelly.lbalance;
+
+import java.util.List;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class LoadBalancer extends Plugin {
+   
+    @Override
+    public void onEnable() {
+        this.getLogger().info("kinda balancing");
+    }
+}
+
