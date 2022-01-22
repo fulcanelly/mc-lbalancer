14bb087065c9ae47fe133c63f3983a068c7f53f6
fulcanelly <dallasxfulcanelly@gmail.com>
2022-01-22 19:59:29 +0200

    add checkExculding

--- src.java/src/main/java/me/fulcanelly/lbalance/ServerOnlineChecker.java
+++ src.java/src/main/java/me/fulcanelly/lbalance/ServerOnlineChecker.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import com.google.common.base.Function;
@@ -34,6 +35,21 @@ public class ServerOnlineChecker {
 
         sinfos.values()
             .forEach(s -> s.ping(this::acceptPing));
+
+        return IntStream.range(0, sinfos.size())
+            .boxed()
+            .map(const_(queue::take))
+            .reduce(true, (a, b) -> a && b);
+    }
+
+    @SneakyThrows
+    boolean checkExcluding(String serverName) {
+
+        var sinfos = server.getServers().values().stream()
+            .filter(it -> ! it.getName().equals(serverName))
+            .collect(Collectors.toList());
+
+        sinfos.forEach(s -> s.ping(this::acceptPing));
 
         return IntStream.range(0, sinfos.size())
             .boxed()
