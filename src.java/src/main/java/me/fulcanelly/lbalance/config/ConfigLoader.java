4053237156bd5ac5087c59875a8ee15ce4b65986
fulcanelly <dallasxfulcanelly@gmail.com>
2022-01-23 03:46:28 +0200

    implement rest of config loader

--- src.java/src/main/java/me/fulcanelly/lbalance/config/ConfigLoader.java
+++ src.java/src/main/java/me/fulcanelly/lbalance/config/ConfigLoader.java
@@ -1,6 +1,8 @@
 package me.fulcanelly.lbalance.config;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
@@ -18,22 +20,49 @@ public class ConfigLoader<T> {
 
 
     @SneakyThrows
-    public T load() {
+    void saveResource() {
+        var is = plugin.getResourceAsStream("config.yml");
+        
+        var fout = new FileOutputStream(getConfigFile());
+        byte []sss;
+        fout.write(sss = is.readAllBytes());
+        System.out.println(sss);
+        fout.close();
+    }
+
+
+    File getConfigFile() {
         var folder = plugin.getDataFolder();
         folder.mkdir();
 
-        var cfile = folder.toPath()
+        return folder.toPath()
             .resolve("config.yml")
-            .toFile();
-          
+            .toFile(); 
+    }
+
+    @SneakyThrows
+    T readYaml() {
+        var yaml = new Yaml(new Constructor(clazz));
+        
+        return yaml.load(
+            new FileInputStream(getConfigFile()));
+    }
+
+    T saveAndRead() {
+        saveResource();
+        return readYaml();
+    }
+
+    @SneakyThrows
+    public T load() {
+        var cfile = getConfigFile();
+
         return switch (cfile.exists() ? Existence.Present : Existence.None) {
             case Present -> new Yaml(new Constructor(clazz))
                 .load(new FileInputStream(cfile));
-            default -> clazz.getConstructor()
-                .newInstance();
+            default -> saveAndRead();
         };    
     } 
-
 }
 
 
